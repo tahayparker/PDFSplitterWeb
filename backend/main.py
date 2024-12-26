a440e17d613ee5801e7a80ce49ab171fde045bdf
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-26 10:46:20 +0400

    update

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://PDFSplitter.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
